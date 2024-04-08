f7a9c2f989a1e9a55cc535827ab1d523e8f46759
Cory Buecker <email@corybuecker.com>
2024-04-08 07:41:57 -0500

    --wip-- [skip ci]

--- DayOne/Playground.swift
+++ DayOne/Playground.swift
@@ -1,13 +1,13 @@
 import Cocoa
 
-let greeting = "Hello, playground!"
+let movies: [String] = [
+  "The Godfather", "The Shawshank Redemption", "Pulp Fiction",
+  "The Lord of the Rings: The Return of the King", "Forrest Gump", "The Dark Knight", "The Matrix",
+  "The Silence of the Lambs", "The Lion King", "The Incredibles", "The Godfather",
+]
 
-print(greeting)
+let uniqueMovies: Set<String> = Set(movies)
 
-let actor = "Tom Cruise"
-
-let quoted = "The \"actor\" is \(actor)"
-
-print(quoted.count)
-
-print(quoted.uppercased())
+print(
+  "There are \(movies.count) movies in the array and \(uniqueMovies.count) unique movies in the set."
+)
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## 2024-04-07 starting over
 
-
+https://www.hackingwithswift.com/quick-start/beginners/how-to-check-a-condition-is-true-or-false
